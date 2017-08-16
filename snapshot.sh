62f7479d6a0a5733368e4550adf469a4ee8d6838
Avelino <t@avelino.xxx>
2017-08-16 17:02:13 -0300

    create gofn.service systemd

---
+++ snapshot.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+
+echo "CREATE DOCKER CUSTOM FILE..."
+mkdir /etc/systemd/system/docker.service.d/
+echo """[Service]
+ExecStart=/usr/bin/dockerd -H tcp://${IP}:2375 -H unix:///var/run/docker.sock
+""" > /etc/systemd/system/docker.service.d/custom.conf
+
+echo "RESTART DOCKER..."
+systemctl daemon-reload && systemctl restart docker
