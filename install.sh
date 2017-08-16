f59275032a7992ead70bcef40bec2c846b8dbcbc
Avelino <t@avelino.xxx>
2017-08-16 17:12:41 -0300

    used curl download file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-https://raw.githubusercontent.com/gofn/snapshot.service/master/snapshot.sh > /usr/local/bin/snapshot.sh
+curl https://raw.githubusercontent.com/gofn/snapshot.service/master/snapshot.sh > /usr/local/bin/snapshot.sh
 chmod +x /usr/local/bin/snapshot.sh
 curl https://raw.githubusercontent.com/gofn/snapshot.service/master/gofn.service > /etc/systemd/system/gofn.service
 systemctl enable gofn.service
