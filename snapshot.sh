b28678994671f65a12f9e4d6f3f066f86cda7853
Avelino <t@avelino.xxx>
2017-08-16 19:37:17 -0300

    service boot after docker.service

--- snapshot.sh
+++ snapshot.sh
@@ -2,8 +2,9 @@
 IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 
 echo "CREATE DOCKER CUSTOM FILE..."
-mkdir /etc/systemd/system/docker.service.d/
+mkdir -p /etc/systemd/system/docker.service.d/
 echo """[Service]
+ExecStart=
 ExecStart=/usr/bin/dockerd -H tcp://${IP}:2375 -H unix:///var/run/docker.sock
 """ > /etc/systemd/system/docker.service.d/custom.conf
 
